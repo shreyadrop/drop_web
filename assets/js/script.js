bdbf9d8fbd833784fd6ecd0d0b16654b58cbe43d
shreyadrop <91486424+shreyadrop@users.noreply.github.com>
2022-03-23 15:39:07 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,7 @@ $(document).ready(function() {
     $('.collapse-target').hide();
     $(document).on('click', '.collapse-trigger', function() {
         // $(this).toggleClass('after-plus after-minus');
+         $('.collapse-target').hide();
         $(this).next().slideToggle();
         return false;
     });
